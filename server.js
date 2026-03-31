e265df8acec9a2ec34b6d8f45e0405d8967e54a0
srbrenoss25-png <srbrenoss25@gmail.com>
2026-03-31 19:52:09 -0300

    Atualizaçao automatica do servidor,/criação do usuario

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 import express from 'express'
+
 const app= express();
+
+
+const users = []
+
+app.post('/usuarios', (req, res,) => {
+
+console.log(req)
+
+res.send('Ok, aqui deu certo')
+
+})
 
 app.get('/usuarios', (request,  res) => {
     res.send('Ok, Deu Certo')
